0251fe2a555d975c0afc902c13acb299c51979f3
thatbeer <6130812021@student.chula.ac.th>
2023-01-05 11:47:41 +0700

    edit card

--- src/components/progressbar.jsx
+++ src/components/progressbar.jsx
@@ -24,22 +24,22 @@ function Progressbar(props) {
                     <div className='flex flex-between w-full divide-x divide-gray-500'>
                         <span className='flex flex-col w-full items-center 
                         justify-start mx-auto my-2'>
-                            <svg class="animate-bounce w-6 h-6 text-amber-500 mx-2 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                            
                             รอส่ง 0/100
                         </span>
                         <span className='flex flex-col w-full
                          items-center justify-start mx-auto my-2'>
-                             <svg class="animate-bounce w-6 h-6 text-amber-500 mx-2 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                             
                             เหลือ 100
                         </span>
                         <span className='flex flex-col w-full
                          items-center justify-start mx-auto my-2'>
-                             <svg class="animate-bounce w-6 h-6 text-amber-500 mx-2 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                             
                             เหลือ 100
                         </span>
                         <span className='flex flex-col w-full
                          items-center justify-start mx-auto my-2'>
-                             <svg class="animate-bounce w-6 h-6 text-amber-500 mx-2 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                             
                             รอส่ง 100
                         </span>
                     </div>
