363b035eba3ffed5b2e4304878918119a5c9d1bc
thatbeer <6130812021@student.chula.ac.th>
2023-01-06 17:36:06 +0700

    add pdf lib

--- src/page/GuidePage.jsx
+++ src/page/GuidePage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
+
 const GuidePage = () => {
   return (
     <>
         <div className='container mx-auto my-4 pt-[60px] text-center'>
-            I am your guide. 
+        
         </div>
     </>
   )
