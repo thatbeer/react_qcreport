f5765b7910267984d6ab8b305849bea2fdd30329
thatbeer <88705394+thatbeer@users.noreply.github.com>
2023-01-07 11:08:08 +0700

    add tooltabs to pdf file

--- src/page/GuidePage.jsx
+++ src/page/GuidePage.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
-import PdfViewerComponent from '../components/pdf/PdfViewerComponent'
+// import PdfViewerComponent from '../components/pdf/PdfViewerComponent'
 import { Viewer , Worker } from "@react-pdf-viewer/core";
 import '@react-pdf-viewer/core/lib/styles/index.css'
 import pdf from '../components/pdf/Guide.pdf'
+import { toolbarPlugin } from '@react-pdf-viewer/toolbar';
+import '@react-pdf-viewer/toolbar/lib/styles/index.css';
+
+
 
 const GuidePage = () => {
+  const toolbarPluginInstance = toolbarPlugin();
+  const { Toolbar } = toolbarPluginInstance;
+
+
+
   return (
     <>
-        <div className='flex flex-1 relative mx-auto pt-[50px] text-center h-full overflow-hidden'>
+        <div className='flex flex-col relative mx-2 pt-[90px] justify-center text-center h-full overflow-hidden'>
           {/* <PdfViewerComponent document="Guide.pdf" /> */}
-          <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.1.81/build/pdf.worker.min.js" >
-            <Viewer fileUrl={pdf} />;
-          </Worker>
+        
+          <Toolbar /> 
+
+            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.1.81/build/pdf.worker.min.js" >
+              <Viewer fileUrl={pdf}  plugins={[ toolbarPluginInstance]}/>;
+            </Worker>
+ 
+    
         </div>
     </>
   )
